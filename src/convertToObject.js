e2c682b19966cd42e8f2d30aa2c42a3151725f48
Yaroslav Mozheitov <129555075+YriKm@users.noreply.github.com>
2025-08-27 13:56:28 +0100

    27.08.2025

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,14 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return Object.fromEntries(
+    sourceString
+      .split(';')
+      .map((a) => a.trim())
+      .filter((a) => a.length > 0)
+      .map((a) => a.split(':'))
+      .map((a) => a.map((b) => b.trim())),
+  );
 }
 
 module.exports = convertToObject;
